ccfcfb6b4ff268578fdefe73c73174957cd7d98d
Andrey <globalshow6@gmail.com>
2021-09-17 22:46:30 +0300

    Сделал окно "О программе"

--- TFCS_ FirstWork/AboutForm.Designer.cs
+++ TFCS_ FirstWork/AboutForm.Designer.cs
@@ -29,12 +29,75 @@ namespace TFCS__FirstWork
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
+            this.NameSurnameText = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // NameSurnameText
+            // 
+            this.NameSurnameText.AutoSize = true;
+            this.NameSurnameText.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NameSurnameText.Location = new System.Drawing.Point(319, 338);
+            this.NameSurnameText.Name = "NameSurnameText";
+            this.NameSurnameText.Size = new System.Drawing.Size(320, 29);
+            this.NameSurnameText.TabIndex = 0;
+            this.NameSurnameText.Text = "Шестаков Андрей БИ-31";
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.CloseButton.AutoSize = true;
+            this.CloseButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CloseButton.ForeColor = System.Drawing.Color.Black;
+            this.CloseButton.Location = new System.Drawing.Point(612, 9);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(27, 25);
+            this.CloseButton.TabIndex = 5;
+            this.CloseButton.Text = "X";
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            this.CloseButton.MouseEnter += new System.EventHandler(this.CloseButton_MouseEnter);
+            this.CloseButton.MouseLeave += new System.EventHandler(this.CloseButton_MouseLeave);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(54, 64);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(238, 233);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = resources.GetString("textBox1.Text");
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(666, 376);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.NameSurnameText);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AboutForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AboutForm";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.AboutForm_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.AboutForm_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label NameSurnameText;
+        private System.Windows.Forms.Label CloseButton;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- TFCS_ FirstWork/AboutForm.cs
+++ TFCS_ FirstWork/AboutForm.cs
@@ -15,6 +15,38 @@ namespace TFCS__FirstWork
         public AboutForm()
         {
             InitializeComponent();
+            CloseButton.BackColor = Color.Transparent;
+            NameSurnameText.BackColor = Color.Transparent;
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        Point lastPoint;
+        private void AboutForm_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void AboutForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void CloseButton_MouseEnter(object sender, EventArgs e)
+        {
+            CloseButton.ForeColor = Color.White;
+        }
+
+        private void CloseButton_MouseLeave(object sender, EventArgs e)
+        {
+            CloseButton.ForeColor = Color.Black;
         }
     }
 }
