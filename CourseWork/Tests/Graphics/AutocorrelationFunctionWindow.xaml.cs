e2df403523d036514304f24a598cad9ce4ebe9af
Андрей Шестаков <GlobalShow6@Gmail.com>
2022-05-28 21:35:02 +0300

    Добавил заготовки для 5 графических тестов и 5 статистических. Добавленно окно вывода всей информации.

---
+++ CourseWork/Tests/Graphics/AutocorrelationFunctionWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourseWork.Tests
+{
+    /// <summary>
+    /// Логика взаимодействия для AutocorrelationFunctionWindow.xaml
+    /// </summary>
+    public partial class AutocorrelationFunctionWindow : Window
+    {
+        public AutocorrelationFunctionWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
